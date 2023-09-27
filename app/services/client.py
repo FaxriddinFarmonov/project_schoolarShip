e7af066011e5517bfad1b103d6e73db8a45e4e3c
user <faxriddin.farmonov.dev@gmail.com>
2023-09-27 13:33:54 +0500

    27 incubatsiya

--- app/services/client.py
+++ app/services/client.py
@@ -8,10 +8,13 @@ from django.shortcuts import render,redirect
 
 def client_doc(request,service:int = None):
     sql=f"""
-    select cr.id as cr_id, cr.name as cr_name, doc.id, doc.name as fname, doc.familya as lname, doc.img,doc.phone,doc.gender  
+    select cr.id as cr_id, cr.name as cr_name, doc.id, doc.name as fname, doc.familya as lname, doc.img,doc.phone,doc.gender,  
+    pro.name as prof,pos.name as 'position'
     from app_service as cr
     inner join app_servicedoc sd on sd.service_id=cr.id
     inner join app_user doc on sd.doc_id=doc.id
+    left join app_position pos on pos.id=doc.position_id
+    left join app_professions pro on pro.id=doc.prof_id
     where cr.id={service}
     
     """
